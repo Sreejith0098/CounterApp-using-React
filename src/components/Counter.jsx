0148dabcec13ee68f896e2b948b9ac10bb87ebab
Sreejith R <rsreejith090@gmail.com>
2025-08-08 14:36:23 +0530

    Counter App Completed

---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const[count,setCount] = useState(0)
+
+    function Increment(){
+         setCount(count+1)
+    }
+    function Decrement(){
+        setCount(count-1)
+    }
+    function Reset(){
+        setCount(0)
+    }
+  return (
+    <div style={{height:'100vh', width:'100%'}} className='d-flex justify-content-center align-items-center' >
+        <div className='border border-2 rounded-3 p-5'  style={{width:'400px'}} >
+     
+            <h1 className='text-center'>Counter App</h1>
+            <h1 className='text-center bold'>{count}</h1>
+           
+            <div className='mt-4'>
+            <button onClick={Increment}  className='btn btn-primary  '>Increment</button>
+            <button onClick={Decrement} className='btn btn-warning ms-2'>Decrement</button>
+            <button onClick={Reset} className='btn btn-danger ms-2'>Reset</button>
+            </div>
+      
+        </div>
+    </div>
+  )
+}
+
+export default Counter
